480ccaf86fc4f703b2ec0c00b0d781ed809c9f32
PhungThanhTu <67744403+PhungThanhTu@users.noreply.github.com>
2021-06-10 21:14:34 +0700

    update UI
    login  screen giờ có dấu X để tắt
    tăng kích thước cửa sổ Register
    tất cả các cửa sổ giờ đây không thể resize

--- Student Enroll Management/UI-for-Project/Model/sqlConnection.cs
+++ Student Enroll Management/UI-for-Project/Model/sqlConnection.cs
@@ -7,6 +7,6 @@ namespace UI_for_Project.Model
     static class sqlConnection
     {
         // NẾU MUỐN SỬA MÃ THÌ SỬA Ở ĐÂY LÀ ĐC NHA
-        public static string CONNECTION = @"Data Source=PCHIEU\SQLEXPRESS;Initial Catalog=QLTuyenSinh;Integrated Security=True";
+        public static string CONNECTION = @"Data Source=PHUNGTHANHTU;Initial Catalog=QLTuyenSinh;Integrated Security=True";
     }
 }
